8d41946698fd51fade0a575745e10ef9eefef5bc
HarishSekar <hasekar@ciber.com>
2019-02-21 19:41:14 -0500

    finished file uploader preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,58 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule, MatNativeDateModule} from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { BaseComponent } from './base/base.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    UploadImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatTooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatBadgeModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
